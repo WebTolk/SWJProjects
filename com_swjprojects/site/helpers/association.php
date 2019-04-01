3223962b16284913017a61b6fe1509fa222f70d8
Septdir <septdir@gmail.com>
2019-04-01 16:47:21 +0300

    Add debug to associations [close #24]

--- com_swjprojects/site/helpers/association.php
+++ com_swjprojects/site/helpers/association.php
@@ -31,6 +31,7 @@ abstract class SWJProjectsHelperAssociation
 	 * @param   string   $view        Name of the view.
 	 * @param   integer  $catid       Id of the category.
 	 * @param   integer  $project_id  Id of the project.
+	 * @param   integer  $debug       Enable debug.
 	 *
 	 * @throws  Exception
 	 *
@@ -38,13 +39,14 @@ abstract class SWJProjectsHelperAssociation
 	 *
 	 * @since  1.0.0
 	 */
-	public static function getAssociations($id = 0, $view = null, $catid = 0, $project_id = 0)
+	public static function getAssociations($id = 0, $view = null, $catid = 0, $project_id = 0, $debug = 0)
 	{
 		$app        = Factory::getApplication();
 		$id         = (!empty($id)) ? $id : $app->input->getInt('id', 0);
 		$view       = (!empty($view)) ? $view : $app->input->getCmd('view', '');
 		$catid      = (!empty($catid)) ? $catid : $app->input->getInt('catid', 1);
 		$project_id = (!empty($project_id)) ? $project_id : $app->input->getInt('project_id', 0);
+		$debug      = (!empty($debug)) ? $debug : $app->input->getInt('debug', 0);
 		$hash       = md5(serialize(array($id, $view, $catid, $project_id)));
 
 		if (!isset(self::$_associations[$hash]))
@@ -75,6 +77,10 @@ abstract class SWJProjectsHelperAssociation
 
 				if ($link)
 				{
+					if (!empty($debug))
+					{
+						$link .= '&debug=1';
+					}
 					$associations[$code] = $link;
 				}
 			}
