9828e5325e6021bfe542c462c17151883000b36d
Septdir <septdir@gmail.com>
2019-05-28 12:51:10 +0300

    Prepare 1.2.1 release

--- com_swjprojects/script.php
+++ com_swjprojects/script.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    SW JProjects Component
- * @version    __DEPLOY_VERSION__
+ * @version    1.2.1
  * @author     Septdir Workshop - www.septdir.com
  * @copyright  Copyright (c) 2018 - 2019 Septdir Workshop. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
@@ -200,7 +200,7 @@ class com_swjprojectsInstallerScript
 	/**
 	 * Method to create hits column if don't exist.
 	 *
-	 * @since  __DEPLOY_VERSION__
+	 * @since  1.2.1
 	 */
 	protected function checkHitsColumn()
 	{
