17f9b8a036fafc87922f167d58817cd4db78360c
Igor Berdichevkskiy <septdir@gmail.com>
2020-03-12 01:13:50 +0300

    Prepare 1.5.4 release

--- com_swjprojects/media/js/field-image.js
+++ com_swjprojects/media/js/field-image.js
@@ -1,6 +1,6 @@
 /*
  * @package    SW JProjects Component
- * @version    __DEPLOY_VERSION__
+ * @version    1.5.4
  * @author     Septdir Workshop - www.septdir.com
  * @copyright  Copyright (c) 2018 - 2020 Septdir Workshop. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
