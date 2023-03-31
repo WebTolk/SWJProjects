887eec202e12f4a39a27b0fa9118127e19475c2a
Sergey Tolkachyov <info@web-tolk.ru>
2023-03-31 11:41:42 +0400

    Revert last commit

--- com_swjprojects/site/views/versions/tmpl/default_j4.php
+++ com_swjprojects/site/views/versions/tmpl/default_j4.php
@@ -79,12 +79,12 @@ use Joomla\CMS\Language\Text;
 						<?php endif; ?>
 					</ul>
 					<div class="text-center">
-						<?php if ($this->project->download_type === 'paid' && $this->project->payment->get('link') && !empty($item->version)): ?>
+						<?php if ($this->project->download_type === 'paid' && $this->project->payment->get('link')): ?>
 							<a href="<?php echo $this->project->payment->get('link'); ?>"
 							   class="btn btn-success col-12">
 								<?php echo Text::_('COM_SWJPROJECTS_BUY'); ?>
 							</a>
-						<?php elseif ($this->project->download_type === 'free' && !empty($item->version)): ?>
+						<?php elseif ($this->project->download_type === 'free'): ?>
 							<a href="<?php echo $this->project->download; ?>" class="btn btn-primary col-12"
 							   target="_blank">
 								<?php echo Text::_('COM_SWJPROJECTS_DOWNLOAD'); ?>
