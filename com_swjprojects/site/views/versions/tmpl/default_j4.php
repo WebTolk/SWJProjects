fe8a0c0b44e1529e5a82f5505bfaff3dd17eb258
Sergey Tolkachyov <info@web-tolk.ru>
2023-03-31 11:47:05 +0400

    Fix #75
    by another way

--- com_swjprojects/site/views/versions/tmpl/default_j4.php
+++ com_swjprojects/site/views/versions/tmpl/default_j4.php
@@ -79,12 +79,12 @@ use Joomla\CMS\Language\Text;
 						<?php endif; ?>
 					</ul>
 					<div class="text-center">
-						<?php if ($this->project->download_type === 'paid' && $this->project->payment->get('link')): ?>
+						<?php if ($this->project->download_type === 'paid' && $this->project->payment->get('link') && !empty($this->project->version)): ?>
 							<a href="<?php echo $this->project->payment->get('link'); ?>"
 							   class="btn btn-success col-12">
 								<?php echo Text::_('COM_SWJPROJECTS_BUY'); ?>
 							</a>
-						<?php elseif ($this->project->download_type === 'free'): ?>
+						<?php elseif ($this->project->download_type === 'free' && !empty($this->project->version)): ?>
 							<a href="<?php echo $this->project->download; ?>" class="btn btn-primary col-12"
 							   target="_blank">
 								<?php echo Text::_('COM_SWJPROJECTS_DOWNLOAD'); ?>
