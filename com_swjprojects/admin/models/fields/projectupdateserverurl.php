d511e2bea3a3e6dd8c99b3053572f289273da8f7
sergeytolkachyov <info@web-tolk.ru>
2023-08-23 18:08:59 +0400

    small cleanup

--- com_swjprojects/admin/models/fields/projectupdateserverurl.php
+++ com_swjprojects/admin/models/fields/projectupdateserverurl.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package     WebTolk plugin info field
- * @version     1.8.0
- * @Author 		Sergey Tolkachyov, https://web-tolk.ru
- * @copyright   Copyright (C) 2020 Sergey Tolkachyov
- * @license     GNU/GPL http://www.gnu.org/licenses/gpl-2.0.html
- * @since 		1.0.0
+ * @package       WebTolk plugin info field
+ * @version       1.8.0
+ * @Author        Sergey Tolkachyov, https://web-tolk.ru
+ * @copyright     Copyright (C) 2020 Sergey Tolkachyov
+ * @license       GNU/GPL http://www.gnu.org/licenses/gpl-2.0.html
+ * @since         1.0.0
  */
 
 defined('_JEXEC') or die;
@@ -46,38 +46,38 @@ class JFormFieldProjectupdateserverurl extends JFormField
 	 */
 	protected function getLabel()
 	{
-	$app = Factory::getApplication();
-	
-	if ($app->input->get('view') == 'project' && !empty($project_id = $app->input->get('id')))
-	
-	{
-		JLoader::register('SWJProjectsHelperRoute', JPATH_SITE . '/components/com_swjprojects/helpers/route.php');
-		
-		BaseDatabaseModel::addIncludePath(JPATH_SITE . '/components/com_swjprojects/models');
-		$model = BaseDatabaseModel::getInstance('Project', 'SWJProjectsModel', array('ignore_request' => false));
-		$project = $model->getItem();
-		$url = Uri::getInstance(Uri::root());
-		
-		$component_params = ComponentHelper::getParams('com_swjprojects');
-				// Join over current translates
-		$lang = $component_params->get('changelogurl_language', 'en-GB');
-		$project_name = $project->translates[$lang]->title;
-		if(empty($project_name)){
-			$project_name = 'Your extension name';
+		$app = Factory::getApplication();
+
+		if ($app->input->get('view') == 'project' && !empty($project_id = $app->input->get('id')))
+		{
+			JLoader::register('SWJProjectsHelperRoute', JPATH_SITE . '/components/com_swjprojects/helpers/route.php');
+
+			BaseDatabaseModel::addIncludePath(JPATH_SITE . '/components/com_swjprojects/models');
+			$model   = BaseDatabaseModel::getInstance('Project', 'SWJProjectsModel', array('ignore_request' => false));
+			$project = $model->getItem();
+			$url     = Uri::getInstance(Uri::root());
+
+			$component_params = ComponentHelper::getParams('com_swjprojects');
+			// Join over current translates
+			$lang         = $component_params->get('changelogurl_language', 'en-GB');
+			$project_name = $project->translates[$lang]->title;
+			if (empty($project_name))
+			{
+				$project_name = 'Your extension name';
+			}
+			$url->setPath(Route::link('site', SWJProjectsHelperRoute::getJUpdateRoute('', $project->element)));
+
+
+			return $html = '</div>
+				<div class="col-12 alert alert-info mt-4">
+				<h4>' . Text::_('COM_SWJPROJECTS_JOOMLA_UPDATE_SERVER') . '</h4>
+				 <p><code>' . $url->toString() . '</code></p>
+				 <p>' . Text::sprintf('COM_SWJPROJECTS_JOOMLA_UPDATE_SERVER_URL_FIELD_DESC', $project_name, $url->toString()) . '</p>
+				</div><div>
+			';
+
 		}
-		$url->setPath(Route::link('site', SWJProjectsHelperRoute::getJUpdateRoute('', $project->element)));
-		
-		
-		return $html = '</div>
-		<div class="col-12 alert alert-info mt-4">
-		<h4>'.Text::_('COM_SWJPROJECTS_JOOMLA_UPDATE_SERVER').'</h4>
-		 <p><code>'.$url->toString().'</code></p>
-		 <p>'.Text::sprintf('COM_SWJPROJECTS_JOOMLA_UPDATE_SERVER_URL_FIELD_DESC',$project_name, $url->toString()).'</p>
-		</div><div>
-		';
-		
-	}
-		
+
 
 	}
 
