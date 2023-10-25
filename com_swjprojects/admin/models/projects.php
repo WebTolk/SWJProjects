7bad70676dd145870654381429e7597c5be33873
Sergey Tolkachyov <musstudent@mail.ru>
2023-10-25 14:35:57 +0400

    fix language constants

--- com_swjprojects/admin/models/projects.php
+++ com_swjprojects/admin/models/projects.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @package    SW JProjects Component
- * @version    1.9.0-alpha
+ * @version    1.9.0
  * @author Septdir Workshop, <https://septdir.com>, Sergey Tolkachyov <https://web-tolk.ru>
  * @сopyright (c) 2018 - October 2023 Septdir Workshop, Sergey Tolkachyov. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
