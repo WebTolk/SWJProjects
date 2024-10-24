14c83fc104580a986a530d14b42a99e7217e1ed5
Sergey Tolkachyov <musstudent@mail.ru>
2024-10-24 11:35:00 +0400

    Если указана внешняя ссылка на документацию - не отображается текст и иконка. Выводим той же кнопкой, что и внутреннюю документацию. Но нужно исключить из массива urls

--- com_swjprojects/layouts/project/urls/documentation.php
+++ com_swjprojects/layouts/project/urls/documentation.php
@@ -23,9 +23,9 @@ extract($displayData);
  *
  */
 
-if ($item->documentation)
+if ($item->documentation || $item->urls->get('documentation'))
 {
-	$link         = $item->documentation;
+	$link         = ($item->documentation ? $item->documentation : $item->urls->get('documentation'));
 	$title        = '<i class="fas fa-file-alt"></i> '.Text::_('COM_SWJPROJECTS_DOCUMENTATION');
 	$link_attribs = [
 		'class' => 'btn btn-outline-info me-2 mb-2'
